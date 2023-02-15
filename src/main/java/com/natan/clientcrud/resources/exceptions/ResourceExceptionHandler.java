943c9d9d0d135aa06cdf70799e4ad792cf3d9ca3
Natan-TI <natan201125@hotmail.com>
2023-02-15 00:57:58 -0300

    POST, PUT and DELETE implemented

--- src/main/java/com/natan/clientcrud/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/natan/clientcrud/resources/exceptions/ResourceExceptionHandler.java
@@ -7,15 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.natan.clientcrud.services.exceptions.EntityNotFoundException;
+import com.natan.clientcrud.services.exceptions.DatabaseException;
+import com.natan.clientcrud.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
@@ -24,5 +25,17 @@ public class ResourceExceptionHandler {
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError("Database exception!");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/natan/clientcrud/services/ClientService.java
+++ src/main/java/com/natan/clientcrud/services/ClientService.java
@@ -5,13 +5,18 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.natan.clientcrud.dto.ClientDTO;
 import com.natan.clientcrud.entities.Client;
 import com.natan.clientcrud.repositories.ClientRepository;
-import com.natan.clientcrud.services.exceptions.EntityNotFoundException;
+import com.natan.clientcrud.services.exceptions.DatabaseException;
+import com.natan.clientcrud.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -28,7 +33,48 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setCpf(dto.getCpf());
+		entity.setChildren(dto.getChildren());
+		entity.setIncome(dto.getIncome());
+		
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setCpf(dto.getCpf());
+			entity.setChildren(dto.getChildren());
+			entity.setIncome(dto.getIncome());
+			
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found! Id:" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found! Id:" + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation!");
+		}
+		
 	}
 }
